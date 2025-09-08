5c8860db20383716bc93581ab81cde7f3c5cd2ed
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-08 11:48:58 -0300

    aula 08/09

--- Python/auto_py.py/automacao.py
+++ Python/auto_py.py/automacao.py
@@ -6,7 +6,34 @@ import pyautogui, time
 # print(time.sleep(3)) -> da um delay para depois o código rodar
 # print(pyautogui.click(x=603, y=888)) -> faz o mouse clicar em algo
 
-print(time.sleep(7))
-print(pyautogui.click(x=214, y=836))
-print(time.sleep(1.5))
-print(pyautogui.click(x=1160, y=272))
+# print(time.sleep(4))
+# print(pyautogui.moveTo(x=276, y=241, duration=1)) -> move o mouse
+
+print(time.sleep(1))
+pyautogui.click(x=604, y=878, duration=1)
+print(time.sleep(1))
+pyautogui.moveTo(x=276, y=241, duration=1)
+pyautogui.click(x=266, y=331, duration=1)
+pyautogui.click(x=468, y=338, duration=1)
+pyautogui.click(x=822, y=308, duration=1)
+
+print(time.sleep(4))
+pyautogui.click(x=99, y=150, duration=1)
+
+pyautogui.click(x=609, y=181, duration=1)
+print(time.sleep(3))
+
+# time.sleep(5)
+# pyautogui.scroll(-500)
+
+pyautogui.write('Monitor Gamer', interval=0.3)
+pyautogui.press('enter')
+
+
+# print(time.sleep(7))
+# print(pyautogui.click(x=214, y=836))
+# print(time.sleep(1.5))
+# print(pyautogui.click(x=1160, y=272))
+
+# time.sleep(3)
+# print(pyautogui.position())
