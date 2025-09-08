ea37ccf09cda86189603257f58f9a7904d17a457
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-08 10:17:16 -0300

    teste

--- Python/auto_py.py/automacao.py
+++ Python/auto_py.py/automacao.py
@@ -6,5 +6,7 @@ import pyautogui, time
 # print(time.sleep(3)) -> da um delay para depois o código rodar
 # print(pyautogui.click(x=603, y=888)) -> faz o mouse clicar em algo
 
-print(time.sleep(3))
+print(time.sleep(7))
 print(pyautogui.click(x=214, y=836))
+print(time.sleep(1.5))
+print(pyautogui.click(x=1160, y=272))
