0b641fa612a502af038ff408a84ecfff5389bcac
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-08 10:13:04 -0300

    Create automacao.py

---
+++ Python/auto_py.py/automacao.py
@@ -0,0 +1,10 @@
+import pyautogui, time
+
+# print(pyautogui.position()) -> posição do mouse na tela
+# print(pyautogui.size()) -> tamanho do munitor
+
+# print(time.sleep(3)) -> da um delay para depois o código rodar
+# print(pyautogui.click(x=603, y=888)) -> faz o mouse clicar em algo
+
+print(time.sleep(3))
+print(pyautogui.click(x=214, y=836))
